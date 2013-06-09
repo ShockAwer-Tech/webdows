ad62f523ab4f692556495492dd393e721a4a9088
c0nrad <sclarsen@mtu.edu>
2013-06-08 23:59:54 -0400

    Added a new application

--- webdows.js
+++ webdows.js
@@ -4,6 +4,18 @@
 
     $( ".closeBtn").click(function() {
     	$(this).parent().parent().parent().hide(500);
+    });
+
+    $( ".minimizeBtn").click(function() {
+    	$(this).parent().parent().parent().find("#content").hide(400);
+    	$(this).parent().parent().parent().height(65);
+
+    })
+
+    $( ".maximizeBtn").click(function() {
+    	$(this).parent().parent().parent().find("#content").show(400);
+    	$(this).parent().parent().parent().height(310);
+
     })
 
 
